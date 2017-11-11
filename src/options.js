024fb92603fc0985c6f43828fb681c3fd5508ca5
sparcut <sparcut@users.noreply.github.com>
2017-11-11 22:22:30 +1100

    added test notification to options

--- src/options.js
+++ src/options.js
@@ -1,5 +1,9 @@
+import dateFormat from 'date-fns/format';
+import { Notifications } from './utils/chrome';
+
 import './sass/options.sass'
 
+// Function Definitions
 const saveOptions = (options) => {
   return new Promise((res, rej) => {
     chrome.storage.sync.get('options', (items = {}) => {
@@ -27,7 +31,22 @@ const setSavingStatus = (status) => {
   }
 }
 
+const testNotification = () => {
+  Notifications.create('test', {
+    type: 'basic',
+    iconUrl: '../assets/icons/purple_128.png',
+    title: `Test notification! (${dateFormat(Date.now(), 'h:mm a')})`,
+    message: 'This notification was generated as a test.',
+    contextMessage: 'BetterIPTV',
+    priority: 2,
+    eventTime: Date.now(),
+    isClickable: true
+  });
+}
+
+// Executed code (starts here)
 const OptionInputs = document.querySelectorAll('.option-input');
+const TestNotificationButton = document.getElementById('test-notification'); 
 
 chrome.storage.sync.get('options', (items = {}) => {
   const options = items.options || {};
@@ -67,3 +86,5 @@ chrome.storage.sync.get('options', (items = {}) => {
     input.addEventListener(inputType, onInputChange());
   });
 });
+
+TestNotificationButton.addEventListener('click', testNotification);
