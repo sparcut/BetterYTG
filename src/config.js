097955da1d439d20b4f6f3f931d909d2c4a8c296
sparcut <sparcut@users.noreply.github.com>
2017-11-12 17:54:57 +1100

    added default options to config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,19 @@ const CONFIG = {
   liveCheck: {
     url: 'http://107.170.95.160/live',
     interval: 30000
+  },
+  defaultOptions: {
+    enableLiveNotification: true,
+    enableNotificationSound: true,
+    notificationVolume: 0.6,
+    enableTwitchEmotes: true,
+    enableTwitchSubEmotes: true,
+    enableBetterTTVEmotes: true,
+    enableIceOldEmotes: true,
+    emoteChannels: 'monkasen, graphistrs, trihex, reckful, b0aty, NightDev',
+    removeChatAvatars: true,
+    enableChatColors: true,
+    separateChatLines: false
   }
 }
 
