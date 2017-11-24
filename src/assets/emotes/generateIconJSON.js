cfe40bfb3180396b4e01595065f14ea509d454a0
sparcut <sparcut@users.noreply.github.com>
2017-11-24 17:59:14 +1100

    fixed custom emote icon names

--- src/assets/emotes/generateIconJSON.js
+++ src/assets/emotes/generateIconJSON.js
@@ -1,15 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-const fileObj = {}
+const mustacheFormat = (string, replacements) => {
+  return string.replace(/\{\{(.*?)\}\}/g, (_, key) => replacements[key]);
+}
+
+const wordReplacements = {
+  'colon': ':'
+}
+
+const fileArr = [];
 
 fs.readdir('.', (err, files) => {
-  files.forEach((f) => {
-    const file = path.posix.parse(f);
+  files.forEach((rawFile) => {
+    const file = path.posix.parse(rawFile);
     if(file.ext === '.png' || file.ext === '.gif') {
-      fileObj[file.name] = f;
+      const needsFormat = file.name.indexOf('{') !== -1;
+      if(needsFormat) {
+        fileArr.push([ mustacheFormat(file.name, wordReplacements), rawFile ]);
+      } else {
+        fileArr.push([ file.name, rawFile ]);
+      }
     }
   });
 
-  fs.writeFile('./dictionary.json', JSON.stringify(fileObj, null, 2), (err) => console.log('done - ' + err));
+  fileArr.sort((a, b) => (
+    a[0] < b[0] ? 
+      -1 : 
+      (a[0] > b[0] ?
+        1 :
+        0)
+  ));
+
+  const JSONString = JSON
+    .stringify(fileArr, null, 2)
+    .replace(/\[\s{5}"/g, '[ "') // Removes whitespace/newline in multi line array (at start)
+    .replace(/",\s{4}/g, '",') // Removes newline in mutli line array (inside array)
+    .replace(/"\s{3}]/g, '" ]'); // Removes whitespace/newline in multi line array (at end)
+
+  fs.writeFile('./dictionary.json', JSONString, (err) => {
+    if(err) return console.log(err);
+    console.log('Done');
+  });
 });
