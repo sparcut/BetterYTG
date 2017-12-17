4c15b122817d28359870b159b13982f09b9bd76c
sparcut <sparcut@users.noreply.github.com>
2017-12-17 18:04:08 +1100

    added popup preact base

---
+++ src/popup/Popup.js
@@ -0,0 +1,17 @@
+import { h, Component } from 'preact';
+
+import Views from './Views';
+
+import PersistentSyncStorage from '';
+
+class Popup extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div id="Popup"></div>
+    );
+  }
+}
--- src/popup/index.js
+++ src/popup/index.js
@@ -1,1 +1,9 @@
-import 'stylus/popup.styl'
+import { h, render } from 'preact';
+import Popup from './Popup'; 
+
+import 'stylus/popup.styl';
+
+render(
+  <Popup />,
+  document.getElementById('container')
+);
